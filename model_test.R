83ff9420cdf329eac53e6d4d81c14797fe8830d6
avril-m-harder <avrilharder@gmail.com>
2021-06-30 15:06:24 -0500

    Update model_test.R

--- model_test.R
+++ model_test.R
@@ -1,23 +1,33 @@
 setwd('/Users/Avril/Documents/summer_lab_coding_project/summer_lab_coding_project/')
 
+### model overview -------------------------
+##
+## init pop --> *age up --> die --> reproduce --> age up --> die --> reproduce --> age up ...
+## 
+## *this is why some individual are intialized as age == -1
+##
+
 ## set parameters -------------------------
 max.age <- 4 ## individuals die before reproducing in year 4 (last repro year = 3)
 brood <- 1 ## brood size (# of offspring per pair? probably needs to be 2 if we're going for replacement)
 k <- 100 ## want to allow population size to vary about k
 n.years <- 50 ## number of years over which to run simulation
+prop.female <- 0.50 ## proportion of individuals that are female in the starting population
+repro.age <- 1 ## age at reproductive maturity (i.e., can reproduce in this year)
+
 
 ## initialize population ------------------
-pop <- matrix(nrow=100, ncol=6)
+pop <- matrix(nrow=k, ncol=6)
 colnames(pop) <- c('id','sex','mom','dad','age','num.off')
-pop[,1] <- seq(1,100,1) ## unique ID for each indiv
-pop[,2] <- sample(c(0,1),100,replace = TRUE) ## randomly assign male/female (0=M, 1=F)
-pop[,3] <- rep(0,100) ## mom ID = 0 for simulation founders
-pop[,4] <- rep(0,100) ## mom ID = 0 for simulation founders
-pop[,5] <- sample(c(-1:3),100,replace=TRUE) ## assign ages (could edit to model specific starting population age structure)
-pop[,6] <- rep(0,100) ## beginning number of offspring is 0 for all indivs
+pop[,1] <- seq(1,k,1) ## unique ID for each indiv
+pop[,2] <- sample(c(0,1),k,replace = TRUE,prob = c(1-prop.female, prop.female)) ## randomly assign male/female (0=M, 1=F)
+pop[,c(3:4)] <- 0 ## mom & dad ID = 0 for simulation founders
+pop[,5] <- sample(c(-1:(max.age-1)),k,replace=TRUE) ## assign ages (could edit to model specific starting population age structure)
+pop[,6] <- rep(0,k) ## beginning number of offspring is 0 for all indivs
 
-
-
+for(y in 1:n.years){
+  pop <- pop[-(sample(c(1:nrow(pop)),1)),]
+}
 
 ### copied from J's RunModel.R:
 # out1 <- matrix(0, ncol = 2, nrow = 10) # create a matrix for outputting data 
