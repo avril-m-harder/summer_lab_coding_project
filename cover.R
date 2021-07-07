5dba5d5c93189ce5eaaa7e847483e9670ec924d9
avril-m-harder <avrilharder@gmail.com>
2021-07-07 15:04:58 -0500

    Update cover.R

--- cover.R
+++ cover.R
@@ -1,12 +1,12 @@
 #setup
-setwd('/Users/Avril/Documents/summer_lab_coding_project/summer_lab_coding_project/')   #set working directory
+setwd('/Users/Avril/Documents/summer_lab_coding_project/summer_lab_coding_project/') ## set working directory
 directory <- getwd()
-outdir    <- paste(directory,"/output/",sep="")                                                    #directory to save model output  
-source(paste0(directory, "/source/FunctionSourcer.R"))                                   #source functions and set source directory
+outdir    <- paste(directory,"/output/",sep="") ## directory to save model output  
+source(paste0(directory, "/source/FunctionSourcer.R")) ## source functions and set source directory
 
 ## set parameters -------------------------
 max.age       <- 4 ## individuals die before reproducing in year 4 (last repro year = 3)
-brood         <- 1 ## brood size (# of offspring per pair? probably needs to be 2 if we're going for replacement)
+brood         <- c(1,2) ## brood size (# of offspring per pair? probably needs to be 2 if we're going for replacement)
 k             <- 100 ## want to allow population size to vary about k
 n.years       <- 50 ## number of years over which to run simulation
 prop.female   <- 0.50 ## proportion of individuals that are female in the starting population
